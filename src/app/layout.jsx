96902f5a591232b6471d0ed7295818fa30d09f0a
Emad <arabanemad@gmail.com>
2025-10-07 15:40:35 +0330

    create:provider & update layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,8 @@
-import ToastProvider from "@/components/ui/ToastProvider";
 import "./globals.css";
 import { fontIranYekan } from "@/lib/font";
+import Providers from "./Providers";
+import ToastProvider from "@/components/ui/ToastProvider";
+
 export const metadata = {
   title: "فروکروم با کیفیت | Ferrochrome Industries",
   description: "تولید و فروش فروکروم با کیفیت ممتاز و تحویل به سراسر دنیا.",
@@ -10,8 +12,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body className={`${fontIranYekan.variable} bg-black text-white`}>
+        <Providers>
           <ToastProvider />
-        {children}
+          {children}
+        </Providers>
       </body>
     </html>
   );
