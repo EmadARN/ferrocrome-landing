ac87a9bcac755141ce37687f6f05570acd9a90f6
Reza <rezafafa78@gmail.com>
2025-10-14 13:19:48 +0330

    trancrate added to AnimatedCard and used in blogs

--- src/components/blogs/AnimatedCard.jsx
+++ src/components/blogs/AnimatedCard.jsx
@@ -5,6 +5,7 @@ import { format } from "date-fns";
 import { faIR } from "date-fns/locale";
 import Image from "next/image";
 import Link from "next/link";
+import truncateText from "@/utils/trancrate";
 
 const AnimatedCard = ({ title, summary, createdAt, image, href }) => {
   const dateObj = createdAt ? new Date(createdAt) : null;
@@ -15,8 +16,9 @@ const AnimatedCard = ({ title, summary, createdAt, image, href }) => {
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
+        style={{ backgroundColor: "var(--color-card-bg)" }}
         whileHover={{ scale: 1.03, boxShadow: "0 10px 20px rgba(0,0,0,0.2)" }}
-        className="bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer h-[400px] flex flex-col"
+        className=" rounded-lg shadow-lg overflow-hidden cursor-pointer h-[400px] flex flex-col"
       >
         {/* تصویر */}
         {image && (
@@ -27,7 +29,10 @@ const AnimatedCard = ({ title, summary, createdAt, image, href }) => {
 
         <div className="p-4 flex flex-col flex-1 overflow-hidden">
           {/* تاریخ پایین عکس */}
-          <span className="text-gray-400 text-sm mb-2">
+          <span
+            style={{ color: "var(--color-title-secondary)" }}
+            className="text-gray-400 text-sm mb-2"
+          >
             🗓{" "}
             {dateObj && !isNaN(dateObj)
               ? format(dateObj, "PPP", { locale: faIR })
@@ -35,17 +40,33 @@ const AnimatedCard = ({ title, summary, createdAt, image, href }) => {
           </span>
 
           {/* عنوان */}
-          <h2 className="text-lg font-bold text-gray-800 mb-2 line-clamp-2">
+          <h2
+            style={{ color: "var(--color-title-secondary)" }}
+            className="text-lg font-bold text-gray-800 mb-2 line-clamp-2"
+          >
             {title}
           </h2>
 
           {/* خلاصه محتوا */}
-          <p className="text-gray-600 text-sm line-clamp-2 flex-1 overflow-hidden">
-            {summary}
+          <p
+            style={{ color: "var(--color-text-secondary)" }}
+            className=" hidden md:flex text-sm  overflow-hidden"
+          >
+            {truncateText(summary, 400)}
+          </p>
+
+          <p
+            style={{ color: "var(--color-text-secondary)" }}
+            className=" text-sm flex-1 md:hidden overflow-hidden"
+          >
+            {truncateText(summary, 270)}
           </p>
 
           {/* ادامه مطلب */}
-          <span className="mt-3 text-blue-600 text-sm font-medium hover:text-blue-800 transition-colors">
+          <span
+            style={{ color: "var(--color-text-secondary)" }}
+            className="mt-3  font-medium hover:text-blue-800 transition-colors"
+          >
             ادامه مطلب →
           </span>
         </div>
